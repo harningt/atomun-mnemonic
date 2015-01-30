fd95967e00f56ad7df7c22d28e8b97c63e77c098
Thomas Harning Jr <harningt@gmail.com>
2015-01-30 01:53:28 -0500

    meta: copyright update

--- src/test/groovy/us/eharning/atomun/mnemonic/LegacyElectrumMnemonicBuilderSpock.groovy
+++ src/test/groovy/us/eharning/atomun/mnemonic/LegacyElectrumMnemonicBuilderSpock.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Thomas Harning Jr. <harningt@gmail.com>
+ * Copyright 2014, 2015 Thomas Harning Jr. <harningt@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
