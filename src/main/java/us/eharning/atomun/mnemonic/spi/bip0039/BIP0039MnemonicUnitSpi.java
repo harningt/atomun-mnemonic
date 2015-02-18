47d9b13dc661e1078af3d0cfd2482b01c376aa18
Thomas Harning Jr <harningt@gmail.com>
2015-02-18 01:40:41 -0500

    bip0039: updates decoding SPI with logic obtained from optimized encoder

--- src/main/java/us/eharning/atomun/mnemonic/spi/bip0039/BIP0039MnemonicUnitSpi.java
+++ src/main/java/us/eharning/atomun/mnemonic/spi/bip0039/BIP0039MnemonicUnitSpi.java
@@ -19,6 +19,8 @@ import com.google.common.base.Charsets;
 import com.google.common.base.Converter;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import com.tomgibara.crinch.bits.BitWriter;
+import com.tomgibara.crinch.bits.ByteArrayBitWriter;
 import us.eharning.atomun.mnemonic.MnemonicUnit;
 import us.eharning.atomun.mnemonic.MnemonicUnitSpi;
 import us.eharning.atomun.mnemonic.spi.BidirectionalDictionary;
@@ -65,14 +67,16 @@ class BIP0039MnemonicUnitSpi extends MnemonicUnitSpi {
      * @param dictionary instance to check for the presence of all words.
      * @param mnemonicWordList sequence of mnemonic words to map against a dictionary for bit values.
      *
-     * @return sequence of bits based on word list.
+     * @return sequence of bytes based on word list.
      */
     @Nonnull
-    private static boolean[] mnemonicToBits(@Nonnull BidirectionalDictionary dictionary, @Nonnull List<String> mnemonicWordList) {
+    private static byte[] mnemonicToBytes(@Nonnull BidirectionalDictionary dictionary, @Nonnull List<String> mnemonicWordList) {
         /* Each word represents 11 bits of entropy (2^11 => 2048 words) */
         int mnemonicSentenceBitCount = mnemonicWordList.size() * 11;
-        boolean[] mnemonicSentenceBits = new boolean[mnemonicSentenceBitCount];
-        int bitIndex = 0;
+        int mnemonicSentenceByteCount = (mnemonicSentenceBitCount + 7) / 8;
+
+        byte[] mnemonicSentenceBytes = new byte[mnemonicSentenceByteCount];
+        BitWriter bitWriter = new ByteArrayBitWriter(mnemonicSentenceBytes);
         Converter<String, Integer> reverseConverter = dictionary.reverse();
         for (String word: mnemonicWordList) {
             /* Find the word index in the wordList. */
@@ -80,38 +84,28 @@ class BIP0039MnemonicUnitSpi extends MnemonicUnitSpi {
             //noinspection ConstantConditions
             int index = reverseConverter.convert(word);
 
-            /* Set the next 11 bits to the value of the index. */
-            for (int i = 0; i < 11; i++) {
-                mnemonicSentenceBits[bitIndex] = (index & (1 << (10 - i))) != 0;
-                bitIndex++;
-            }
+            bitWriter.write(index, 11);
         }
-        return mnemonicSentenceBits;
+        bitWriter.flush();
+        return mnemonicSentenceBytes;
     }
 
     /**
      * Extract the entropy portion from the mnemonic sentence bits as a byte array.
      *
-     * @param mnemonicSentenceBits container of entropy bits.
+     * @param mnemonicSentenceBytes container of entropy bytes.
+     * @param mnemonicSentenceBitCount number of bits representative of sequence.
      *
      * @return array of bytes containing the entropy.
      */
     @Nonnull
-    private static byte[] extractEntropy(@Nonnull boolean[] mnemonicSentenceBits) {
-        int checksumBitCount = mnemonicSentenceBits.length / 33;
-        int entropyBitCount = mnemonicSentenceBits.length - checksumBitCount;
+    private static byte[] extractEntropy(@Nonnull byte[] mnemonicSentenceBytes, int mnemonicSentenceBitCount) {
+        int checksumBitCount = mnemonicSentenceBitCount / 33;
+        int entropyBitCount = mnemonicSentenceBytes.length * 8 - checksumBitCount;
 
         /* Extract original entropy as bytes. */
         byte[] entropy = new byte[entropyBitCount / 8];
-        int bitIndex = 0;
-        for (int i = 0; i < entropy.length; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                if (mnemonicSentenceBits[bitIndex]) {
-                    entropy[i] |= 1 << (7 - j);
-                }
-                bitIndex++;
-            }
-        }
+        System.arraycopy(mnemonicSentenceBytes, 0, entropy, 0, entropy.length);
         return entropy;
     }
 
@@ -119,21 +113,20 @@ class BIP0039MnemonicUnitSpi extends MnemonicUnitSpi {
      * Verify that the entropy matches the checksum embedded in the mnemonic sentence bits.
      *
      * @param entropy bytes to validate checksum of.
-     * @param mnemonicSentenceBits container of checksum bits.
+     * @param mnemonicSentenceBytes container of checksum bits.
      *
      * @return true if the checksum matches, else false.
      */
-    private static boolean verifyEntropyMatch(byte[] entropy, boolean[] mnemonicSentenceBits) {
-        int entropyBitCount = entropy.length * 8;
-        int checksumBitCount = entropyBitCount / 32;
+    private static boolean verifyEntropyMatch(byte[] entropy, byte[] mnemonicSentenceBytes, int mnemonicSentenceBitCount) {
+        int checksumBitCount = mnemonicSentenceBitCount / 32;
         // Take the digest of the entropy.
         byte[] hash = BIP0039MnemonicUtility.sha256digest(entropy);
 
-        // Check all the checksum bits.
+        /* TODO: Optimize hash check to use masks to bulk check. */
         for (int i = 0; i < checksumBitCount; ++i) {
             int byteIndex = i / 8;
             int bitIndex = i % 8;
-            if (mnemonicSentenceBits[entropyBitCount + i] != ((hash[byteIndex] & (1 << (7 - bitIndex))) != 0)) {
+            if ((mnemonicSentenceBytes[entropy.length + byteIndex] & (1 << (7 - bitIndex))) != ((hash[byteIndex] & (1 << (7 - bitIndex))))) {
                 return false;
             }
         }
@@ -151,14 +144,14 @@ class BIP0039MnemonicUnitSpi extends MnemonicUnitSpi {
     @Override
     public byte[] getEntropy(@Nonnull CharSequence mnemonicSequence) {
         List<String> mnemonicWordList = Splitter.onPattern(" |\u3000").splitToList(mnemonicSequence);
-        /* Convert the word list into a sequence of booleans representing its bits. */
-        boolean[] mnemonicSentenceBits = mnemonicToBits(dictionary, mnemonicWordList);
+        int mnemonicSentenceBitCount = mnemonicWordList.size() * 11;
+        byte[] mnemonicSentenceBytes = mnemonicToBytes(dictionary, mnemonicWordList);
 
         /* Extract from the bits the entropy prefix */
-        byte[] entropy = extractEntropy(mnemonicSentenceBits);
+        byte[] entropy = extractEntropy(mnemonicSentenceBytes, mnemonicSentenceBitCount);
 
         /* Verify that the word list is valid using the checksum at the end of the data */
-        if (!verifyEntropyMatch(entropy, mnemonicSentenceBits)) {
+        if (!verifyEntropyMatch(entropy, mnemonicSentenceBytes, mnemonicSentenceBitCount)) {
             throw new IllegalArgumentException("Checksum does not match");
         }
         return entropy;
